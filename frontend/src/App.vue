47ce4cf09b77bb0e1ca073ea299e87ea6abd2481
Didio Perdomo <didioarbey@gmail.com>
2021-09-13 23:24:49 -0500

    arreglos en el frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <inicio/>
+  </div>
+</template>
+
+<script>
+import Inicio from './components/inicio.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Inicio
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul {
+  background: #3399ff;
+  padding: 20px;
+}
+
+ul li {
+  background: #cce5ff ;
+  padding: 5px;
+  margin-left: 35px;
+
+}
+</style>
